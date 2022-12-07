c1d57095ad662ab876d5bf4362bf287a631d65c2
Lsenn1 <lsenn@lion.lmu.edu>
2022-12-06 17:53:32 -0800

    finished all jest test suites and added onto inquirer prompts for employee creation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
+const employees = []
 
 //TODO - write your inquirer app here to gather information about the team members, and generate the HTML file using fs
 function newEmployee(){
@@ -20,7 +21,12 @@ function newEmployee(){
         },
         {
             type: 'list',
-            name: 'Email',
+            name: 'name',
+            message: 'What is the Name of the employee?',
+        },
+        {
+            type: 'list',
+            name: 'email',
             message: 'What is the email of the employee?',
         },
         {
@@ -28,7 +34,7 @@ function newEmployee(){
             name: 'id',
             message: 'What is the id of the employee?',
         }
-    ]).then(({position }) => {
+    ]).then(({position, name, email, id}) => {
         switch(position) {
             case 'Manager':
                 inquirer.prompt([
@@ -37,9 +43,44 @@ function newEmployee(){
                         name: 'officeNumber',
                         message: 'What is the office number?'
                     }
-                ])
+                ]).then(({officeNumber}) => {
+                    employees.push(new Manager(
+                        name,
+                        id,
+                        email,
+                        officeNumber
+                    ))
+                })
             case 'Intern':
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'school',
+                        message: 'What school did this intern go to?'
+                    }
+                ]).then(({school}) => {
+                    employees.push(new Intern(
+                        name,
+                        id,
+                        email,
+                        school
+                    ))
+                })
             case 'Engineer':
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'github',
+                        message: 'What is the github of this engineer?'
+                    }
+                ]).then(({github}) => {
+                    employees.push(new Engineer(
+                        name,
+                        id,
+                        email,
+                        github
+                    ))
+                })
             default:
             
         }
